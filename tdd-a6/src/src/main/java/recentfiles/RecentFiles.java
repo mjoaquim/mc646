3c448d315307ee9ab123d0ef0e1987d741cfd031
Mateus Joaquim <mateusjoaquim@estudiovh.com.br>
2020-12-20 20:07:56 -0300

    green: list size

--- tdd-a6/src/src/main/java/recentfiles/RecentFiles.java
+++ tdd-a6/src/src/main/java/recentfiles/RecentFiles.java
@@ -9,6 +9,7 @@ import java.util.*;
 public class RecentFiles {
 
     private final List<File> elements = new ArrayList<>();
+    private static final int MAX_SIZE_LIST = 10;
 
     public List<File> getList(){
         return elements;
@@ -17,5 +18,12 @@ public class RecentFiles {
     public void addFile(final File file) {
         elements.remove(file);
         elements.add(0, file);
+        reduce();
+    }
+
+    private void reduce() {
+        while(elements.size() > MAX_SIZE_LIST) {
+            elements.remove(elements.size() - 1);
+        }
     }
 }
